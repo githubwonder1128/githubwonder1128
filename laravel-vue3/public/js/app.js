730810fd47b4a06be3842a8d596ad33fd1393863
githubwonder1128 <crazywonderrabbit1128@gmail.com>
2022-05-12 18:11:32 -0700

    update

--- laravel-vue3/public/js/app.js
+++ laravel-vue3/public/js/app.js
@@ -19796,7 +19796,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
 function render(_ctx, _cache) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("h1", null, "About Page");
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("h1", null, "About Git2 Page");
 }
 
 /***/ }),
